d6d8fad51a10c5f40babe445209b634718b83785
AnBC22 <104400577+AnBC22@users.noreply.github.com>
2023-08-24 10:00:37 -0600

    Improved accessibility and modified javascript

--- blog-posts.js
+++ blog-posts.js
@@ -1,56 +1,65 @@
 import { blogPosts } from './data.js'
 
-const viewMoreEl = document.getElementById('view-more')
+console.log("loading initial... ")
+document.addEventListener('DOMContentLoaded', function() {
 
-viewMoreEl.addEventListener('click', renderPosts)
-
-let postsToRender = 3; 
-let currentPage = 1 
-
-function handleWindowSizeChange() {
-    const screenWidth = window.innerWidth;
     
-    if(screenWidth >= 550 && screenWidth < 768) {
-        postsToRender = 4;
-    }
-    else if(screenWidth >= 768) {
-        postsToRender = 6;
-    }
-}
-
-// -------------------------------------
-function renderPosts() {
-
-    const startIndex = (currentPage - 1) * postsToRender;
-    const endIndex = startIndex + postsToRender;
-
-    if(startIndex < blogPosts.length) {
-        document.getElementById('feed').innerHTML = getPostsHtml(startIndex, endIndex)
-        currentPage++;
-    }
-}
-
-// -------------------------------------
-function getPostsHtml(start, end) {
-    let feed = ``
-
-    for(let i = start; i < end && i < blogPosts.length; i++) {
-
-        let post = blogPosts[i]
-
-        feed += `
-            <section class="post">
-                <img class="post-img" src="${post.img}">
-                <p class="post-date">${post.date}</p>
-                <h2>${post.title}</h2>
-                <p>${post.text}</p>
-            </section>
-        ` 
+    let postsToRender = 3; 
+    let currentPage = 1 
+    
+    function handleWindowSizeChange() {
+        const screenWidth = window.innerWidth;
+        
+        if(screenWidth >= 550 && screenWidth < 768) {
+            postsToRender = 4;
+        }
+        else if(screenWidth >= 768) {
+            postsToRender = 6;
+        }
     }
     
-    return feed
-}
-// -------------------------------------
+    // -------------------------------------
+    const viewMoreEl = document.getElementById('view-more') 
 
-handleWindowSizeChange()
-renderPosts()
+    viewMoreEl.addEventListener('click', renderPosts)
+
+    function renderPosts() {
+    
+        const startIndex = (currentPage - 1) * postsToRender;
+        const endIndex = startIndex + postsToRender;
+    
+        if(startIndex < blogPosts.length) {
+            document.getElementById('feed').innerHTML = getPostsHtml(startIndex, endIndex)
+            currentPage++;
+        }
+    }
+    
+    // -------------------------------------
+    function getPostsHtml(start, end) {
+        let feed = ``
+    
+        for(let i = start; i < end && i < blogPosts.length; i++) {
+    
+            let post = blogPosts[i]
+    
+            feed += `
+                <section class="post">
+                    <img class="post-img" src="${post.img}" alt="${post.alt}">
+                    <p class="post-date">${post.date}</p>
+                    <h2>${post.title}</h2>
+                    <p>${post.text}</p>
+                </section>
+            ` 
+        }
+        
+        return feed
+    }
+    // -------------------------------------
+    
+    handleWindowSizeChange()
+    renderPosts()
+    
+});
+  
+
+
--- data.js
+++ data.js
@@ -3,36 +3,42 @@ export const blogPosts = [
         date: 'JULY 23, 2022',
         title: 'Blog one',
         img: 'images/pic01.jpg',
-        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+        alt: 'A computer with code showing on the screen'
     },
     {
         date: 'JUNE 23, 2022',
         title: 'Blog two',
         img: 'images/pic02.jpg',
-        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+        alt: 'A keyboard with letters and symbols in blue'
     },
     {
         date: 'MAY 23, 2022',
         title: 'Blog three',
         img: 'images/pic03.jpg',
-        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+        alt: 'A computer with code showing on the screen'
     },
     {
         date: 'APRIL 23, 2022',
         title: 'Blog four',
         img: 'images/pic04.jpg',
-        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+        alt: 'A laptop half closed in a dark background'
     },
     {
         date: 'MARCH 23, 2022',
         title: 'Blog five',
         img: 'images/pic05.jpg',
-        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+        alt: 'A note with a lightbulb symbolizing an idea'
     },
     {
         date: 'JANUARY 23, 2022',
         title: 'Blog six',
         img: 'images/pic06.jpg',
-        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+        alt: 'An old computer with office articles surrounding it'
     }
 ]
